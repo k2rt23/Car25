842f394d623baf1a7cc24a6defb0e2580edffaf5
LAPTOP-8BL927LI\kartt <karttriin@gmail.com>
2025-01-02 22:36:25 +0200

    added car model and database context

---
+++ Car25/Car25/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CarManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car25.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Car25/Car25/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CarManager.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
+

6cb42195ccc6a9f6e6123c77e5bfbadf05fc0c2b
LAPTOP-8BL927LI\kartt <karttriin@gmail.com>
2025-01-05 20:15:17 +0200

    no message

---
+++ Car25/Cars.Application/CarService.cs
@@ -0,0 +1,27 @@
+﻿using Cars.Core;
+using Cars.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cars.ApplicationServices
+{
+    public class CarService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CarService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Car> GetAllCars()
+        {
+            return _context.Cars.ToList();
+        }
+
+        public IEnumerable<Car> FilterCarsByMake(string make)
+        {
+            return _context.Cars.Where(c => c.Make == make).ToList();
+        }
+    }
+}
---
+++ Car25/Cars.Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cars.Core; 
+namespace Cars.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
